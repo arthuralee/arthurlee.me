f1d20bd59231b5f64221f08495c4c6dce2bceebb
Arthur Lee <arthur@arthurlee.me>
2016-10-23 17:54:40 -0700

    Factored out reflux

--- client/src/views/stores/CardStore.js
+++ client/src/views/stores/CardStore.js
@@ -1,49 +1,37 @@
-var Reflux = require('reflux');
-var CardActions = require('../actions/CardActions.js');
-var CardDB = require('./CardDB.js');
+import CardDB from './CardDB';
 
 const NUMCARDS_PER_LOAD = 3;
 
-module.exports = (function() {
-
-  var CardStore = Reflux.createStore({
-    listenables: [CardActions],
-    getInitialState: function() {
-      this.cards = [];
-      return this.cards;
-    },
-    init: function() {
-      this.currentCard = 0;
-      this.listenTo(CardActions.load, this.fetchData);
-    },
-    fetchData: function() {
-      this.ordering = CardDB.getDefaultOrdering();
-      this.prepareCards(this.cards, this.currentCard, NUMCARDS_PER_LOAD);
+export default class CardStore {
+  constructor(trigger) {
+    this.cards = [];
+    this.currentCard = 0;
+    this.trigger = trigger;
+  }
+  fetchData() {
+    this.ordering = CardDB.getDefaultOrdering();
+    this.prepareCards(this.cards, this.currentCard, NUMCARDS_PER_LOAD);
+    this.trigger(this.cards);
+  }
+  loadMore = (done) => {
+    window.setTimeout(function() {
+      var didRunOutOfCards = this.prepareCards(this.cards, this.currentCard, NUMCARDS_PER_LOAD);
       this.trigger(this.cards);
-    },
-    onLoadMore: function(done) {
-      window.setTimeout(function() {
-        var didRunOutOfCards = this.prepareCards(this.cards, this.currentCard, NUMCARDS_PER_LOAD);
-        this.trigger(this.cards);
-        done(didRunOutOfCards);
-      }.bind(this), 200);
-    },
-    prepareCards: function(cards, index, length) {
-      var didRunOut = true;
-      for (var i=index; i<index+length; i++) {
-        var key = this.ordering[i];
-        if(key) {
-          var nextCard = CardDB.getCard(key);
-          cards.push(nextCard);
-          this.currentCard++;
+      done(didRunOutOfCards);
+    }.bind(this), 200);
+  }
+  prepareCards(cards, index, length) {
+    var didRunOut = true;
+    for (var i=index; i<index+length; i++) {
+      var key = this.ordering[i];
+      if(key) {
+        var nextCard = CardDB.getCard(key);
+        cards.push(nextCard);
+        this.currentCard++;
 
-          didRunOut = false;
-        }
+        didRunOut = false;
       }
-      return didRunOut;
     }
-  });
-
-  return CardStore;
-
-})();
+    return didRunOut;
+  }
+}
